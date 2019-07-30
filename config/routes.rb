3c6d8d57b0c4901f95fa91cc40c84f18b6c46944
s-k <one.day.burst@gmail.com>
2019-07-30 14:30:55 +0000

    chapter15 by 524P

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   resource :account, only: [:show, :edit, :update]
   resource :password, only: [:show, :edit, :update]
 
-  resources :articles
+  resources :articles, only: [:index, :show]
   resources :entries do
     patch "like", "unlike", on: :member
     get "voted", on: :collection
@@ -32,5 +32,6 @@ Rails.application.routes.draw do
     resources :members do
       get "search", on: :collection
     end
+    resources :articles
   end
 end
